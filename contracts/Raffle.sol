c3d8651b649f475a3738372246d313fd0c608a21
Aliceonly <1139045289@qq.com>
2023-09-04 19:36:31 +0800

    Automation performUpKeep

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,10 +9,10 @@ import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.s
 error Raffle__NotEnoughETHEntered();
 error Raffle__TransferFailed();
 error Raffle__NotOpen();
+error Raffle__upkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
 
-contract Raffle is VRFConsumerBaseV2 {
-
-    enum RaffleState{
+contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
+    enum RaffleState {
         OPEN,
         CALCULATING
     }
@@ -26,6 +26,8 @@ contract Raffle is VRFConsumerBaseV2 {
     uint32 private immutable i_callbackGasLimit;
     uint32 private constant NUM_WORDS = 1;
     RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
 
     address private s_rencentWinner;
     // uint256 private s_state; // 0 - pending; 1 - open; 2 - closed; 3 - calculating
@@ -39,7 +41,8 @@ contract Raffle is VRFConsumerBaseV2 {
         uint256 entranceFee,
         bytes32 gasLine,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
@@ -47,6 +50,8 @@ contract Raffle is VRFConsumerBaseV2 {
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
         s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     function enterRaffle() public payable {
@@ -56,7 +61,31 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RaffleEnter(msg.sender);
     }
 
-    function requestRandomWinner() external {
+    function fulfillRandomWords(uint256, uint256[] memory randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable rencentWinner = s_players[indexOfWinner];
+        s_rencentWinner = rencentWinner;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
+        (bool success, ) = rencentWinner.call{value: address(this).balance}("");
+        if (!success) revert Raffle__TransferFailed();
+        emit WinnerPicked(rencentWinner);
+    }
+
+    function checkUpkeep(
+        bytes memory
+    ) public view override returns (bool upkeepNeeded, bytes memory) {
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = s_players.length > 0;
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (isOpen && hasPlayers && hasBalance && timePassed);
+    }
+
+    function performUpkeep(bytes calldata) external override {
+        (bool upkeepNeeded,) = checkUpkeep("");
+        if (!upkeepNeeded) revert Raffle__upkeepNotNeeded(address(this).balance, s_players.length, uint256(s_raffleState));
         s_raffleState = RaffleState.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLine,
@@ -66,22 +95,6 @@ contract Raffle is VRFConsumerBaseV2 {
             NUM_WORDS
         );
         emit RequestedRaffleWinner(requestId);
-    }
-
-    function fulfillRandomWords(uint256, uint256[] memory randomWords) internal override {
-        uint256 indexOfWinner = randomWords[0] % s_players.length;
-        address payable rencentWinner = s_players[indexOfWinner];
-        s_rencentWinner = rencentWinner;
-        s_raffleState = RaffleState.OPEN;
-        (bool success, ) = rencentWinner.call{value: address(this).balance}("");
-        if (!success) revert Raffle__TransferFailed();
-        emit WinnerPicked(rencentWinner);
-    }
-
-    function checkUpkeep(
-        bytes calldata
-    ) external view override returns (bool upkeepNeeded, bytes memory performData){
-
     }
 
     function getEntranceFee() public view returns (uint256) {
